587e8e192161b21c33f805392fdeff8d135c389d
SDK-NETWORKS <mohammadsadik894@gmail.com>
2024-09-30 13:46:09 +0530

    Update ConfigMaster.sh

--- ConfigMaster.sh
+++ ConfigMaster.sh
@@ -94,20 +94,20 @@ if [ "$choice" = "1" ]; then
   if [ "$protocol" = "1" ]; then
     clear
     banner
-    python3 Single_Telnet.py
+    python3 modules/Single_Telnet.py
   elif [ "$protocol" = "2" ]; then
     clear
     banner
-    python3 Single_SSH.py
+    python3 modules/Single_SSH.py
   fi
 else
   if [ "$protocol" = "1" ]; then
     clear
     banner
-    python3 Multiple_telnet.py
+    python3 modules/Multiple_telnet.py
   elif [ "$protocol" = "2" ]; then
     clear
     banner
-    python3 Multiple_SSH.py
+    python3 modules/Multiple_SSH.py
   fi
 fi
